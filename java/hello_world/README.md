9787263f6c34915906b7150d69213cd42114fed7
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2013-04-28 18:47:27 -0600

    added java hello world

---
+++ java/hello_world/README.md
@@ -0,0 +1,5 @@
+To run this example:
+
+----
+javac -cp com/evolutionnext/HelloWorld.java   
+java -cp . com.evolutionnext.HelloWorld
---
+++ java/hello_world/com/evolutionnext/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.evolutionnext;
+
+public class HelloWorld {
+   public static void main(String[] args) {
+     System.out.println("Hello World");
+   }
+}
