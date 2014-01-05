5f18a19e352bf06413afa055393ed5642b6908a3
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-01-04 22:19:08 -0700

    added other PatternMatching, cleaned up examples

--- haskell/functors/Functors.hs
+++ haskell/functors/Functors.hs
@@ -7,3 +7,10 @@ instance Functor' Maybe where
    fmap' f (Just x) = (Just (f x))
    fmap' _ Nothing = Nothing
 
+-- Creating an employee data type using record syntax
+data Employee { id :: String
+              , firstName :: String
+              , lastName :: String
+	      , phone :: String
+	      } deriving (Show)
+
--- haskell/pattern_matching/pattern_match.hs
+++
@@ -1,2 +0,0 @@
-myNot True = False
-myNot False = True
