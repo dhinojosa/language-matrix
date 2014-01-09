fd13adc1ee5a9dcf493ca1eb7f02b0d08c4b20e9
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-01-09 10:00:59 -0700

    updated Functions.clj

--- clojure/functions/Functions.clj
+++ clojure/functions/Functions.clj
@@ -1,3 +1,7 @@
-(apply + [a b c])
- ; is the same as
-(+ a b c)
+;This is a function that is created in clojure
+(defn average
+  [numbers]
+    (/ (apply + numbers) (count numbers)))
+
+;To call the function, invoke the symbol `average`
+(println (average [1.0 3.0 10.4 11.5]))
