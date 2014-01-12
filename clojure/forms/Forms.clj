2aa3b0219ad8d2c06f3e3e735755b7d0e48be67b
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-01-11 20:19:41 -0700

    added forms

---
+++ clojure/forms/Forms.clj
@@ -0,0 +1,18 @@
+; Chunks of data read by the clojure reader.
+
+; A number is a form
+(println 22)
+
+; A Vector is a form
+(println [1 2 3])
+
+; A List is a form
+(println 1 2 3)
+
+; A List with a function is also a form
+; Having the + in the beginning is prefix notation
+(println (+ 1 2 4))
+
+;A function with no arguments returns 0
+(println (+))
+
