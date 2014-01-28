7635ef39219e4206d992d79f2a111850656ceebf
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-01-28 10:04:31 -0700

    added binding

---
+++ clojure/binding/Binding.clj
@@ -0,0 +1,9 @@
+; Any call to def or defn (See functions)
+; that object is stored in a var.
+;
+; def binds a function or data to a name
+; this is also called a root binding
+
+(def x 3)
+
+(assert (= x 3))
