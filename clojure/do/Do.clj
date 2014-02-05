d1e3906aa502677641cb6ba2511f31083859239e
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-02-05 13:25:17 -0700

    added Do examples

--- clojure/do/Do.clj
+++ clojure/do/Do.clj
@@ -1,7 +1,27 @@
-; do evaluates all of the expression provided but 
+; `do` evaluates all of the expression provided but 
 ; yields the last expression;
-(do 
-  (println "do something")
-  (apply * [1 2 3]))
+(def x 
+  (do
+     (println "This should be printed as a side effect, while x gets assigned to 1 * 2 * 3")
+     (apply * [1 2 3])
+  )
+)
+(assert (= x 6))
 
+; `do` also marks where a side effect will be performed.  This mimics
+; the idea presented by Haskell. It is also great to perform a unit of work
+(defn is-odd? [number]
+  (if
+    (not= (mod number 2) 0)
+    (do
+       (println (str "found an odd number of " number))
+       true
+    )
+    false
+  )
+)
+
+
+(assert (= (is-odd? 3) true))
+(assert (= (is-odd? 4) false))
 
