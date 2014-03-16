89abdea1b83d47bbfb17ea4582b10e2e62f117dc
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-03-15 22:23:46 -0600

    added defn methods for destructuring

--- clojure/destructuring/destructuring.clj
+++ clojure/destructuring/destructuring.clj
@@ -28,13 +28,15 @@
                 (= b "Milk")
                 (= others ["Lettuce" "Spinach" "Pork Chops"]))))
 
-; Destructuring a vector in an defn
+; Destructuring a vector with an arg as a defn
 (require '[clojure.string :as cs])
-(defn format-grocery-list-1 
+(defn format-grocery-list
    "Format a grocery list with the first two elements, and the rest"
    [[a b & others]]
    (str a " and " b ". If you have time get: " (cs/join ", " others)))
-(assert (= (format-grocery-list-1 grocery-vector) "Eggs and Milk. If you have time get: Lettuce, Spinach, Pork Chops"))
+
+(assert (= (format-grocery-list grocery-vector)
+   "Eggs and Milk. If you have time get: Lettuce, Spinach, Pork Chops"))
 
 ; Using an :as keyword can be used as an alias for the entire vector, especially
 ; after destructuring
@@ -44,13 +46,32 @@
                 (= all-groceries ["Eggs" "Milk" "Lettuce" "Spinach" "Pork Chops"])
                 (= others ["Lettuce" "Spinach" "Pork Chops"]))))
 
-; Destructuring can be done with a vector
+; Using an :as keyword in a defn
+(defn format-grocery-list
+   "Format a grocery list with the first two elements, and the rest"
+   [[a b & others :as all-groceries]]
+   (str "Out of the entire list of " (cs/join ", " all-groceries) "; obtain most importantly " 
+        a " and " b ". If you have time get " (cs/join ", " others)))
+
+(assert (= (format-grocery-list grocery-vector)
+           (str "Out of the entire list of Eggs, Milk, Lettuce, Spinach, Pork Chops;"
+                " obtain most importantly Eggs and Milk. If you have time get Lettuce, Spinach, Pork Chops")))
+
+
+; Destructuring can be done with a map
 (def elvis {:first-name "Elvis" :last-name "Presley" :middle-name "Aaron"})
 
 (let [{fname :first-name lname :last-name} elvis]
   (assert (and (= fname "Elvis")
                (= lname "Presley"))))
 
+; Destructuring a map in a defn
+(defn format-elvis
+   "Format the elvis map"
+   [{fname :first-name lname :last-name}]
+   (str "Elvis' first name was " fname " and his last was " lname))
+
+(assert (= (format-elvis elvis) "Elvis' first name was Elvis and his last was Presley"))
 
 ; The above used `fname` and `lname` to show destructuring but mostly
 ; it used with the same name, this essentially converts keywords to variables
@@ -58,10 +79,31 @@
   (assert (and (= first-name "Elvis")
                (= last-name "Presley"))))
 
-; The last two examples are so used that there is a directive that creates variable
+; Destructuring the previous example in a defn
+
+(defn format-elvis
+   "Format the elvis map"
+   [{first-name :first-name last-name :last-name}]
+   (str "Elvis' first name was " first-name " and his last was " last-name))
+
+(assert (= (format-elvis elvis) "Elvis' first name was Elvis and his last was Presley"))
+
+; The last two examples are so used that there is a directive that destructuring variables using default
+; names
+
 (let [{:keys [first-name last-name]} elvis]
   (assert (and (= first-name "Elvis")
                (= last-name  "Presley"))))
+
+; Destructuring the previous example in a defn
+
+(defn format-elvis
+   "Format the elvis map"
+   [{:keys [first-name last-name]}]
+   (str "Elvis' first name was " first-name " and his last was " last-name))
+
+(assert (= (format-elvis elvis) "Elvis' first name was Elvis and his last was Presley"))
+
 
 ; The :as on a vector binds a variable to an entire vector in the groceries example above,
 ; but it can also be bound to an entire map
@@ -70,11 +112,32 @@
                (= last-name  "Presley"))
                (= king-of-rock-and-roll elvis)))
 
+; Destructuring the previous example in a defn
+
+(defn format-elvis
+   "Format the elvis map"
+   [{:keys [first-name last-name] :as king-of-rock-and-roll}]
+   (str "Elvis' first name was " first-name
+        " and his last was " last-name ". Map size was "
+        (count king-of-rock-and-roll) "."))
+
+(assert (= (format-elvis elvis) "Elvis' first name was Elvis and his last was Presley. Map size was 3."))
+
+
 ; Destructuring can contain default values if some are not available
 (let [{:keys [first-name last-name age] :or {age 0}} elvis]
    (assert (and (= first-name "Elvis")
                 (= last-name "Presley")
                 (= age 0))))
+
+; Destructuring the previous example in a defn
+(defn format-elvis
+   "Format the elvis map"
+   [{:keys [first-name last-name age] :or {age 0}}]
+   (str "Elvis' first name was " first-name " and his last was " last-name ". His age is " age "."))
+
+(assert (= (format-elvis elvis) "Elvis' first name was Elvis and his last was Presley. His age is 0."))
+
 
 ; Destructuring with all options available options
 (let [{:keys [first-name last-name age] :or {age 0} :as king-of-rock-and-roll} elvis]
@@ -82,6 +145,16 @@
                 (= last-name "Presley")
                 (= age 0)
                 (= king-of-rock-and-roll elvis))))
+
+; Destructuring the previous example in a defn
+(defn format-elvis
+   "Format the elvis map"
+   [{:keys [first-name last-name age] :or {age 0} :as king-of-rock-and-roll}]
+   (str "Elvis' first name was " first-name " and his last was "
+        last-name ". His age is " age ". Map size was "
+        (count king-of-rock-and-roll) "."))
+
+(assert (= (format-elvis elvis) "Elvis' first name was Elvis and his last was Presley. His age is 0. Map size was 3."))
 
 ; Destructuring a nested map
 (def elvis {:first-name "Elvis" :last-name "Presley" :middle-name "Aaron"
@@ -91,6 +164,17 @@
 (let [{first-name :first-name {home-name :name} :address} elvis]
     (assert (and (= first-name "Elvis")
                  (= home-name "Graceland"))))
+
+; Destructuring the previous example in a defn
+(defn format-elvis
+   "Format the elvis nested map"
+   [{first-name :first-name {home-name :name} :address}]
+   (str "Elvis' first name was " first-name
+        ". His home name was "
+        home-name "."))
+
+(assert (= (format-elvis elvis) "Elvis' first name was Elvis. His home name was Graceland."))
+
 
 ; Destructuring a nested map using keys; remember keys only bind the keyword
 ; Therefore `name` must be used instead of `home-name`
