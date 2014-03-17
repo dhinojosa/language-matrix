b726fe5e6c17d41218550c36013c12b9e6cb2bb5
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-03-17 11:28:02 -0600

    added some examples to sequences

--- clojure/sequences/sequences.clj
+++ clojure/sequences/sequences.clj
@@ -1,14 +1,13 @@
 ; A list, set, map are sequences and can be operated on
 ; by a common set of functions.  Keep in mind that all 
 ; sequence functions may return an `ISeq`, a java representation 
-; of a `Seq` (sequence)
+; of a `Seq` (sequence) pronounced 'Seek'
 ;
 ; first obtains the first element
 (assert (= (first '(1 2 3 4 5)) 1))
 
 ; rest obtains the collection minus the first element
 ; the seq function will return a `seq` of any sequence.
-;
 (assert (= (rest #{1 2 3 4 5}) (seq #{4 3 2 5})))
 
 ; cons will append an element depending on the collectiona
@@ -26,9 +25,7 @@
 (assert (= (first [5 6 7 9]) 5))
 
 ; maps are seq-able
-;
 (def contact-info {:first-name "Elvis" :last-name "Presley" :address "Graceland" :city "Memphis" :state "TN"})
-
 
 ; Operations that return collections are different 
 ; depending on the collection being operated on.
@@ -42,6 +39,15 @@
 ; An operation on a set returns a clojure.lang.APersistentMap$KeySeq
 (assert (= (. (class (rest #{1 2 3 4})) getName) "clojure.lang.APersistentMap$KeySeq"))
 
+; An operation on a map returns a clojure.lang.Aclojure.lang.PersistentHashMap
+(assert (= (. (class (rest contact-info)) getName) "clojure.lang.PersistentHashMap$NodeSeq"))
 
+; Many of these operations wouldn't make sense based on implementation like hash maps and sets 
+; since the order cannot be determined at runtime.  You can however use collections like sorted set 
+; and sorted map see sorted_sets/sortedsets.clj and sorted_maps/sortedmaps.clj for more information
 
+(def star-trek-characters (sorted-set "Spock" "Kirk" "Worf" "Picard" "Riker"))
+(assert (first star-trek-characters) "Spock")
 
+(def band-hometowns (sorted-map :depeche-mode :basildon :the-cure :crawley :talking-heads :providence))
+(assert (first band-hometowns) [:depeche-mode :basildon])
