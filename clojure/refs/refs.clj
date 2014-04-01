5ee2f3466508961fe6c209f834e1d1dc71c99fbe
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-04-01 13:32:49 -0600

    updated refs with alter and commune

--- clojure/refs/refs.clj
+++ clojure/refs/refs.clj
@@ -21,3 +21,51 @@
 ;
 (dosync (ref-set current-roll [1 2]))
 (assert (= (deref current-roll) [1 2]))
+
+; To update two separate refs, wrap two ref-sets in a `dosync`
+
+(def checking (ref 1000))
+(def savings (ref 20000))
+
+(dosync
+  (ref-set checking (+ (deref checking) 100))
+  (ref-set savings (- (deref savings) 100)))
+
+(assert (and
+           (= (deref checking) 1100)
+           (= (deref savings) 19900)))
+
+; While the previous examples work, there is a better way using `alter`
+; Alter will perform a read and write operation in a single step by applying 
+; a function to referenced object in a transaction. The function must be one that accepts
+; the ref as the first argument.
+
+(dosync
+  (alter checking + 100)
+  (alter savings - 100))
+
+(assert (and
+          (= (deref checking) 1200)
+          (= (deref savings) 19800)))
+
+
+; alter will perform operation in the order that they are give, commute will
+; allow for more concurrency since they will occur in any order
+
+(def log (ref []))
+(dosync
+  (alter log conj 1)
+  (alter log conj 2)
+  (alter log conj 3)
+  (alter log conj 4))
+
+(assert (= (deref log) [1 2 3 4]))
+
+; (def log (ref []))
+; (dosync
+;   (commute log conj 1)
+;   (commute log conj 2)
+;   (commute log conj 3)
+;   (commute log conj 4))
+; 
+; (assert (not (= (deref log) [1 2 3 4])))
