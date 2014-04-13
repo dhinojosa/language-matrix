d2999fba0c8d6c30cbe835518d81b75ec655c62d
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-04-12 19:17:05 -0600

    added more ref examples

--- clojure/refs/refs.clj
+++ clojure/refs/refs.clj
@@ -1,12 +1,16 @@
 ; `ref` is a mutable reference. Clojure is immutable in just about every
 ; regard, but when mutability is required, then ref wraps around 
 ; a reference that can potentially be mutable. `ref` protects the state.
+; `ref` is synchronous.
 
 ; Modelling the roll of some dice
 (def current-roll (ref [6 3]))
 
-; Used `deref` to read the contents
+; Use `deref` to read the contents
 (assert (= (deref current-roll) [6 3]))
+
+; The @<ref-name> can also be used to `deref`
+(assert (= @current-roll [6 3]))
 
 ; If we do not use `deref`, you will merely get the ref object
 (assert (= (. (class current-roll) getName) "clojure.lang.Ref"))
@@ -32,8 +36,8 @@
   (ref-set savings (- (deref savings) 100)))
 
 (assert (and
-           (= (deref checking) 1100)
-           (= (deref savings) 19900)))
+           (= @checking 1100)
+           (= @savings 19900)))
 
 ; While the previous examples work, there is a better way using `alter`
 ; Alter will perform a read and write operation in a single step by applying 
@@ -61,11 +65,29 @@
 
 (assert (= (deref log) [1 2 3 4]))
 
+; TODO: Come back to this one when we do agents and threads, or put in that category
 ; (def log (ref []))
 ; (dosync
-;   (commute log conj 1)
-;   (commute log conj 2)
-;   (commute log conj 3)
-;   (commute log conj 4))
-; 
+;   (doseq [x (range 0 4)]
+;     (commute log conj (do
+;                          (let [rand-num (* (rand-int 2000) x)]
+;                              (println rand-num)
+;                              (Thread/sleep rand-num)
+;                              (+ x 1))))
+;   )
+; )
+; (println (deref log))
 ; (assert (not (= (deref log) [1 2 3 4])))
+
+; refs can be validated when added as an option to a ref using `:validator.`
+; `validator` requires a function that takes one argument.
+
+(def is-even #(= (mod % 2) 0))
+
+(def my-number (ref 0 :validator is-even)) ; uses the function is-even to 
+                                           ; validate reference
+
+(dosync
+  (alter my-number + 2))
+
+(assert (= (deref my-number) 2))
