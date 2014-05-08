8f0c1de804617748f78ea34a399bdf06572edbdc
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-05-08 12:32:11 -0600

    updated binding, will need to update binding as well

--- clojure/vars/Vars.clj
+++ clojure/vars/Vars.clj
@@ -1,14 +1,32 @@
 ; Anything declared as a `def` for value names 
-; or `defn` for function names can be referred by a var
+; or `defn` for function names can be referred by a var.
+; Any def or def n calls are a root binding available to all threads.
 
 (def x 10)
 
-(defn funx [] "Up on a hill")
+; locating x in a separate thread
+(.start (Thread. #(assert (= x 10))))
+
+; dynamic is a macro that allows a var to be reassigned in a binding
+(def ^:dynamic y 10)
+
+; inside the binding scope `y` takes on the value of `11`, the var `y` had
+; previous been declared as `^:dynamic`
+
+(binding [y 11]
+  (assert (= y 11)))
+
+; the binding should no take effect outside of the scope of the binding
+; therefore y should be 10
+(assert (= y 10))
+
 
 ; The initial value of a var is called a root binding
 
 (assert (= (var x) #'user/x))         ; var will not return the value but the binding
                                       ; the #' is a reader macro that does the same thing
+
+(defn funx [] "Up on a hill")
 (assert (= (var x) #'x))
 (assert (= (var funx) #'user/funx))
 (assert (= (var funx) #'funx))
