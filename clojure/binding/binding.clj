6cfb7f16a11ad32f27807f5d7f7d1c069ffdbda9
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-05-12 12:41:42 -0600

    bindings updated with correct information

--- clojure/binding/binding.clj
+++ clojure/binding/binding.clj
@@ -7,3 +7,24 @@
 (def x 3)
 
 (assert (= x 3))
+
+; binding will re-bind the var into another value inside of
+; for a different form context. This will perform anything with in the binding
+; in parallel which differs from `let`. You can't do it this though
+; if the root binding is not declared dynamic as it will throw a 
+; `IllegalStateException`
+
+(try 
+   (binding [x 5] (+ x 5))
+   (catch IllegalStateException e (.getMessage e)))
+
+; the remedy to the situation declare the root binding with 
+; the ^:dynamic macro annotation
+
+(def ^:dynamic y 3)
+
+(assert (= y 3))
+
+(binding [y 5] (assert (= y 5)))
+
+
