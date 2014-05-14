837379abcecf2f3ea9edc5d8a5c6e49f10306d7c
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-05-14 08:36:37 -0600

    updated binding with some new stuff

--- clojure/binding/binding.clj
+++ clojure/binding/binding.clj
@@ -1,6 +1,6 @@
 ; Any call to def or defn (See functions)
 ; that object is stored in a var.
-;
+
 ; def binds a function or data to a name
 ; this is also called a root binding
 
@@ -11,11 +11,12 @@
 ; binding will re-bind the var into another value inside of
 ; for a different form context. This will perform anything with in the binding
 ; in parallel which differs from `let`. You can't do it this though
-; if the root binding is not declared dynamic as it will throw a 
+; if the root binding is not declared dynamic as it will throw a
 ; `IllegalStateException`
 
 (try 
-   (binding [x 5] (+ x 5))
+   (binding [x 5] 
+     (+ x 5))
    (catch IllegalStateException e (.getMessage e)))
 
 ; the remedy to the situation declare the root binding with 
@@ -25,6 +26,16 @@
 
 (assert (= y 3))
 
-(binding [y 5] (assert (= y 5)))
+(binding [y 5]
+  (assert (= y 5)))
 
+; bindings can also redefine methods
 
+(defn ^:dynamic max-1 [a b] (max a b))
+
+(defn ^:dynamic max-2 [a b] (min a b))
+
+; switching the implmentation of max-1 to max-2
+
+(binding [max-1 max-2]
+  (assert (= (max-1 1 3) 1)))
