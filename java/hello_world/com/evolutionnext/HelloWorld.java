cf08f185e73031077bb9f91a4b71f1d8e6e5733e
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-08-18 09:52:23 -0600

    Changed HelloWorld using Java7 feature

--- java/hello_world/com/evolutionnext/HelloWorld.java
+++ java/hello_world/com/evolutionnext/HelloWorld.java
@@ -1,7 +1,7 @@
 package com.evolutionnext;
-
+import static System.out.*;
 public class HelloWorld {
    public static void main(String[] args) {
-     System.out.println("Hello World");
+     println("Hello World");
    }
 }
