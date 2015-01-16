c31d12bbbb8b6e626f20a6e8fc4619f29caf78d4
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2015-01-16 12:05:51 -0700

    Updated Functors.hs with a plausible example

--- haskell/functors/Functors.hs
+++ haskell/functors/Functors.hs
@@ -1,15 +1,22 @@
--- Functors want types that take on one parameter types: `* -> *`
+import Control.Exception.Base (assert)
 
-class Functor' f where
-   fmap' :: (a -> b) -> f a -> f b
+-- Functors want types that take a function of a -> b and 
+-- an instance of that functor with the same type parameter
+-- as the first function argument.
+
+class Functor' fun where
+   fmap' :: (a -> b) -> fun a -> fun b
+
+
+-- Here f is an arbitrary function.  Maybe is an algebraic type that
+-- contains two type constructors, Just or Nothing.
 
 instance Functor' Maybe where
    fmap' f (Just x) = (Just (f x))
    fmap' _ Nothing = Nothing
 
--- Creating an employee data type using record syntax
-data Employee { id :: String
-              , firstName :: String
-              , lastName :: String
-              , phone :: String
-              } deriving (Show)
+-- The ($) function is called function applicator. 
+-- See function-application/Function.hs
+
+main = do
+         putStrLn $ assert (fmap' (1+) (Just 4) == Just 5) "Yes"
