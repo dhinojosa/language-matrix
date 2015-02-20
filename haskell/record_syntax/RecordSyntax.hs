5cc810903e46f9caacb20ed9b539241e564a2389
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2015-02-19 21:26:11 -0700

    updated RecordSyntax.hs

---
+++ haskell/record_syntax/RecordSyntax.hs
@@ -0,0 +1,23 @@
+-- Using an algebraic type but with record syntax to model 
+-- alternate construction, in this case
+-- various ways to create an Employee, `Show` is a type class that provides an
+-- automatic string descriptor.
+import Data.Maybe
+
+type FirstName = String
+type LastName = String
+type MiddleName = Maybe String -- including Maybe type, which is an optional type
+type SSN = String
+type Address = [String]
+
+data Employee = Worker {ssn :: SSN, firstName:: FirstName, middleName :: MiddleName,
+                        lastName :: LastName, addresses :: Address}
+              | Supervisor {ssn :: SSN, firstName :: FirstName, middleName :: MiddleName, 
+                            lastName :: LastName, addresses:: Address, 
+                            employees:: [Employee]} deriving (Show)
+
+main = do
+         let johnTheWorkHorse = Worker "333-12-1200" "John" Nothing "Smith" ["1234 Amber Lane", "St. Louis", "MO"]
+         let bobTheBoss = Supervisor "124-55-3010" "Bob" Nothing "Ramirez" ["33 Main St.", "Skokie", "IL"] [johnTheWorkHorse]
+         putStrLn(firstName(johnTheWorkHorse))
+         putStrLn(firstName(bobTheBoss))
