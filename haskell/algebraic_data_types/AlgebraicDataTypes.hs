230ae0f7ef16109f37007b07f71c504dd5ce9acf
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2015-03-14 13:54:27 -0500

    Got rid of AlgebraicDataType.hs

--- haskell/algebraic_data_types/AlgebraicDataTypes.hs
+++ haskell/algebraic_data_types/AlgebraicDataTypes.hs
@@ -1,6 +1,7 @@
 -- Using an algebraic type to model alternate construction, in this case
 -- various ways to bill, `Show` is a type class that provides an
--- automatic string descriptor.
+-- automatic string descriptor, see 
+
 import Data.Maybe
 
 type FirstName = String
@@ -10,16 +11,23 @@ type SSN = String
 type Address = [String]
 
 data Employee = Worker SSN FirstName MiddleName LastName Address
-              | Supervisor SSN FirstName MiddleName LastName Address [Employee] deriving (Show)
+              | Supervisor SSN FirstName MiddleName LastName 
+                Address [Employee] deriving (Show)
 
 -- Using pattern matching, pattern_matching/PatternMatching.hs for more detail
 firstName :: Employee -> String
 firstName (Worker _ fst _ _ _)  = fst
 firstName (Supervisor _ fst _ _ _ _) = fst
 
-
 main = do
-         let johnTheWorkHorse = Worker "333-12-1200" "John" Nothing "Smith" ["1234 Amber Lane", "St. Louis", "MO"]
-         let bobTheBoss = Supervisor "124-55-3010" "Bob" Nothing "Ramirez" ["33 Main St.", "Skokie", "IL"] [johnTheWorkHorse]
+         let johnTheWorkHorse = Worker "333-12-1200" "John" Nothing 
+                                "Smith" ["1234 Amber Lane", "St. Louis", "MO"]
+         let bobTheBoss = Supervisor "124-55-3010" "Bob" Nothing "Ramirez" 
+                                ["33 Main St.", "Skokie", "IL"] [johnTheWorkHorse]
+ 
          putStrLn(firstName(johnTheWorkHorse))
          putStrLn(firstName(bobTheBoss))
+
+         -- Using function application, see function_application/FunctionApplication.hs
+         putStrLn $ firstName $ johnTheWorkHorse
+         putStrLn $ firstName $ bobTheBoss
