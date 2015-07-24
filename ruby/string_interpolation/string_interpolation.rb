ffa89ccc85d6d497c7c42f7aec36e8ced3ddb997
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2015-07-24 11:12:35 -0600

    added arrays and string_interpolation into ruby

---
+++ ruby/string_interpolation/string_interpolation.rb
@@ -0,0 +1,11 @@
+# A String can be interpolated with a double quoted string
+# Given a couple variable assignments...
+
+name = "Albert Einstein"
+age = 87
+
+# It can be interpolated in the string with a hash and bracket
+# Anything within the brackets will be replaced with the variable
+# values in the context
+
+puts "His name was #{name} and he lived until he was #{age}"
