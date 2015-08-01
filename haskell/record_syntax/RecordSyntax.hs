8e2d29599d5c07c47235de6f09c7a8112f67b65e
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2015-07-31 20:14:27 -0700

    added function application examples to record syntax

--- haskell/record_syntax/RecordSyntax.hs
+++ haskell/record_syntax/RecordSyntax.hs
@@ -1,8 +1,13 @@
--- Using an algebraic type but with record syntax to model 
--- alternate construction, in this case
--- various ways to create an Employee, `Show` is a type class that provides an
--- automatic string descriptor.
+-- Using an algebraic type but with record syntax to avoid creating pattern 
+-- matching to get information from the type. In this case various ways to
+-- create an Employee with functions to obtain items.
+--
+-- `Show` is a type class that provides an automatic string descriptor. 
+-- This is the same example from algebraic_data_types/AlgebraicDataTypes.hs 
+-- but simplified
+
 import Data.Maybe
+import Control.Exception.Base (assert)
 
 type FirstName = String
 type LastName = String
@@ -21,6 +26,13 @@ main = do
          let carlTheSlouch = Worker{ssn = "001-30-3032", addresses = ["803 Amber Lane", "Apt B", "Little Rock", "AR"], firstName = "Carl",
                                     middleName = (Just "Landeau"), lastName = "McInroy"}
          let bobTheBoss = Supervisor "124-55-3010" "Bob" Nothing "Ramirez" ["33 Main St.", "Skokie", "IL"] [johnTheWorkHorse, carlTheSlouch]
-         putStrLn(firstName(johnTheWorkHorse))
-         putStrLn(firstName(bobTheBoss))
-         putStrLn(firstName(carlTheSlouch))
+
+         putStrLn(show(assert(firstName johnTheWorkHorse == "John") "Success"))
+         putStrLn(show(assert(firstName bobTheBoss == "Bob") "Success"))
+         putStrLn(show(assert(firstName carlTheSlouch == "Carl") "Success"))
+
+         -- cleaned using function application. See 
+         -- function_application/FunctionApplication.hs for details
+         putStrLn $ show $ assert (firstName johnTheWorkHorse == "John") "Success"
+         putStrLn $ show $ assert (firstName bobTheBoss == "Bob") "Success"
+         putStrLn $ show $ assert (firstName carlTheSlouch == "Carl") "Success"
