f7789f009d5786d325c9705dc0554c0d33f454e7
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-01-19 12:52:25 -0700

    Cleaned up comments in currying

--- haskell/currying/Currying.hs
+++ haskell/currying/Currying.hs
@@ -1,4 +1,5 @@
 -- Any function can be curried by mere supply part of the arguments
+-- This is automatic currying which is always available in Haskell
 
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
