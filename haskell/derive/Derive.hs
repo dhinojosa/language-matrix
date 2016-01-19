cd6b868f60b5a39bf595153815d4b3e619ba2b61
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-01-19 12:55:49 -0700

    Cleaned up derive

--- haskell/derive/Derive.hs
+++ haskell/derive/Derive.hs
@@ -16,19 +16,34 @@ data Employee = Employee {firstName :: String, lastName :: String} deriving Eq
 -- The following data declaration sets up equality without using Eq but setting
 -- up our own typeclass
 
-data Book = Book {isbn :: String, author :: String, title :: String, chapters :: Int}
+data Book = Book {isbn :: String, author :: String, title :: String,
+                  chapters :: Int}
 
--- Instantiating the type class for Book, since it is not declared with `deriving (Eq)`
+-- Instantiating the type class for Book, since it is not declared with
+-- `deriving (Eq)`
 instance Eq Book where
    a == b = isbn(a) == isbn(b)
 
 main = do
-         let ceo = Employee "Larry" "Brin" -- see let/Let.hs for more on let
-         let someGuyNamedLarry = Employee "Larry" "Brin" -- same values should be equal
-         assert (firstName(ceo) == "Larry") (putStrLn "Larry Brin's first name is indeed Larry")
-         assert (ceo == someGuyNamedLarry) (putStrLn "Larry Brin is indeed Larry")
-         let book = Book "9788498722734" "Harper Lee" "To Kill a Mockingbird" 32
-         let sameBook = Book "9788498722734" "Harper Lee" "To Kill a Mockingbird" 32
-         let sameBookDifferentISBN = Book "9783499142819" "Harper Lee" "To Kill a Mockingbird" 32
-         assert (book == sameBook) (putStrLn "Both `To Kill a Mockingbird books match!")
-         assert (book /= sameBookDifferentISBN) (putStrLn "Both `To Kill a Mockingbird books with different ISBNs do not match match!")
+         -- see let/Let.hs for more on let
+         let ceo = Employee "Larry" "Brin"
+         -- same values should be equal
+         let someGuyNamedLarry = Employee "Larry" "Brin"
+
+         assert (firstName(ceo) == "Larry")
+            (putStrLn "Larry Brin's first name is indeed Larry")
+         assert (ceo == someGuyNamedLarry)
+            (putStrLn "Larry Brin is indeed Larry")
+
+         let book = Book "9788498722734" "Harper Lee"
+                         "To Kill a Mockingbird" 32
+         let sameBook = Book "9788498722734" "Harper Lee"
+                             "To Kill a Mockingbird" 32
+         let sameBookDifferentISBN = Book "9783499142819" "Harper Lee"
+                                     "To Kill a Mockingbird" 32
+         assert (book == sameBook)
+            (putStrLn "Both `To Kill a Mockingbird books match!")
+
+         assert (book /= sameBookDifferentISBN)
+            (putStrLn "Both `To Kill a Mockingbird books with different" ++
+                            " ISBNs do not match match!")
