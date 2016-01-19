573b7c8736d404584851396f0ae1f78a90e11187
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-01-19 12:47:13 -0700

    Cleaned up Applicative Functors to 80 columns

--- haskell/applicative_functors/ApplicativeFunctors.hs
+++ haskell/applicative_functors/ApplicativeFunctors.hs
@@ -20,7 +20,8 @@ import Control.Applicative -- Required for Applicatives!
 --   pure  :: a -> f a
 --   (<*>) :: f (a -> b) -> f a -> f b
 -- 
--- So now we can take Applicatives (which are also functors and apply one to the other!
+-- So now we can take Applicatives (which are also functors
+-- and apply one to the other!
 --
 -- Using a Maybe Applicative, which has the following instance implementation:
 -- instance Applicative Maybe where  
@@ -28,25 +29,29 @@ import Control.Applicative -- Required for Applicatives!
 --     Nothing <*> _ = Nothing  
 --     (Just f) <*> something = fmap f something 
 
-maybeTest1 = assert ((pure 3 :: Maybe Int) == Just 3)              "Maybe Applicative Test 1: ✓"
-maybeTest2 = assert ((Just(\x -> x + 4) <*> Nothing) == Nothing)   "Maybe Applicative Test 2: ✓"
-maybeTest3 = assert ((Just(\x -> x + 4) <*> pure 3) == Just 7)     "Maybe Applicative Test 3: ✓"
-maybeTest4 = assert ((pure (+) <*> Just 4 <*> Just 10) == Just 14) "Maybe Applicative Test 4: ✓"
+maybeTest1 = assert ((pure 3 :: Maybe Int) == Just 3)              "Success"
+maybeTest2 = assert ((Just(\x -> x + 4) <*> Nothing) == Nothing)   "Success"
+maybeTest3 = assert ((Just(\x -> x + 4) <*> pure 3) == Just 7)     "Success"
+maybeTest4 = assert ((pure (*) <*> Just 4 <*> Just 10) == Just 40) "Success"
+maybeTest5 = assert ((pure (+) <*> Just 4 <*> Just 10) == Just 14) "Success"
 
 -- Applicative also has another method that looks very 
 -- similar to function application ($)
 -- (<$>) :: (Functor f) => (a -> b) -> f a -> f b  
 -- f <$> x = fmap f x  
 
-applicativeApplyTest = assert (((* 10) <$> Just 4) == Just 40)      "Applicative Apply Test 1: ✓"
+applicativeApplyTest = assert (((* 10) <$> Just 4) == Just 40)      "Success"
 
 -- Using the List Applicative instance implementation:
 -- instance Applicative [] where  
 --     pure x = [x]
 --     fs <*> xs = [f x | f <- fs, x <- xs] (Note: List comprehension)
 
-listTest1 = assert (([(+ 10), (+ 20)] <*> [1,2,3]) == [11, 12, 13, 21, 22, 23]) "List Applicative Test 1: ✓"
-listTest2 = assert (([(*), (+)] <*> [1,2] <*> [5,4]) == [5, 4, 10, 8, 6, 5, 7, 6]) "List Applicative Test 2: ✓"
+listTest1 = assert (([(+ 10), (+ 20)] <*> [1,2,3]) == [11, 12, 13, 21, 22, 23])
+               "Success"
+listTest2 = assert (([(*), (+)] <*> [1,2] <*> [5,4]) ==
+               [5, 4, 10, 8, 6, 5, 7, 6])
+                  "Success"
 
 -- Using the Function Applicative instance implementation:
 -- instance Applicative ((->) r) where  
@@ -54,15 +59,16 @@ listTest2 = assert (([(*), (+)] <*> [1,2] <*> [5,4]) == [5, 4, 10, 8, 6, 5, 7, 6
 --    f <*> g = \x -> f x (g x)   
 
 pure2Function = (pure 4 :: ((->) r) Int) 
-functionTest1 = assert ((pure2Function 4) == 4)  "Function Applicative Test 1: ✓"
-functionTest2 = assert (((+) <$> (+1) <*> (*10) $ 4) == 45) "Function Applicative Test 2: ✓"
-functionTest3 = assert (((\x y z -> [x,y,z]) <$> (+3) <*> (*2) <*> (/2) $ 5) == [8.0, 10.0, 2.5]) 
-                        "Function Applicative Test 3: ✓"
+functionTest1 = assert ((pure2Function 4) == 4)  "Success"
+functionTest2 = assert (((+) <$> (+1) <*> (*10) $ 4) == 45) "Success"
+functionTest3 = assert (((\x y z -> [x,y,z]) <$> (+3) <*> (*2) <*> (/2) $ 5)
+                          == [8.0, 10.0, 2.5]) "Success"
 
 -- Note the reason why (+1) <*> (*10) by itself will not work is:
 -- Given let z = (+1) <*> (*10) that will translate to (\x -> (+1) x ((*10) x))
--- Therefore: z(4) = (5 (40)), but there no operator, and will not work
--- (+) <$> (+1) <*> (*10) will work because (+) will be applied to the two applicatives
+-- Therefore: z(4) = (5 (40)), but there is no operator, and will not
+-- work and result in a type error (+) <$> (+1) <*> (*10) will work because
+-- (+) will be applied to the two applicatives
 -- Therefore the (5 40) will be summed with (+) to return 45.
  
 main = do
@@ -70,6 +76,7 @@ main = do
          putStrLn $ maybeTest2
          putStrLn $ maybeTest3
          putStrLn $ maybeTest4
+         putStrLn $ maybeTest5
          putStrLn $ applicativeApplyTest
          putStrLn $ listTest1
          putStrLn $ listTest2
