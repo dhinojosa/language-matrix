6cefb4b2bcdcc8dff2d03bf5eb55bc85b423801c
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-01-21 12:59:10 -0700

    Cleaned up Where.hs to include the custom made assertions to make it a bit saner.

--- haskell/where/Where.hs
+++ haskell/where/Where.hs
@@ -1,15 +1,8 @@
-import Control.Exception (assert)
-
--- String is a type synonym for [Char]
-assertFor :: Bool -> String -> String
-assertFor b s = assert b (concat ["Assertion of (", s, "): Passed"])
-
-
-assertStrLn :: Bool -> String -> IO()
-assertStrLn b s = putStrLn $ show $ assertFor b s
+import Util.Assert -- customized library for assertions
 
 -- where can be used much like let and with pattern matching
 -- compare and contrast with let/Let.hs
+
 myMax :: (Ord a) => [a] -> Maybe a
 myMax [] = Nothing
 myMax [x] = Just x
@@ -20,11 +13,7 @@ myMax (x:xs)
             Just last = myMax $ xs -- Pattern matched here
 
 main = do
-         assertStrLn (myMax [1,5,13,42,5,3,1,-3,100,22,10,-40] == Just 100) 
-                     "myMax $ [1,5,13,42,5,3,1,-3,100,22,10,-40] == Just 100"
-         assertStrLn (myMax ([] :: [Int]) == Nothing) 
-                     "myMax ([] :: [Int]) == Nothing" -- Empty List of Int
-         assertStrLn (myMax [1] == Just 1) 
-                     "myMax [1] == Just 1"
-         assertStrLn (myMax [-3,4,1] == Just 4) 
-                     "myMax [-3,4,1] == Just 4"
+         assertThat (myMax [1,5,13,42,5,3,1,-3,100,22,10,-40]) (Just 100) 
+         assertThat (myMax ([] :: [Int])) Nothing 
+         assertThat (myMax [1]) (Just 1) 
+         assertThat (myMax [-3,4,1]) (Just 4)
