cfd3e072eb69cacd9b41a417189ed1dafab7b7b7
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2016-01-26 13:23:15 -0700

    finally added some better example for defrecord

--- clojure/defrecord/defrecord.clj
+++ clojure/defrecord/defrecord.clj
@@ -3,4 +3,7 @@
 (defrecord Person [first-name last-name])
 
 ; Creating a record
+(def louis-armstrong (Person. "Louis" "Armstrong"))
+
+(assert (= (:first-name louis-armstrong) "Louis")) ; Use a keyword first-name
 
