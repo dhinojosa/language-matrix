9201189c9d3acb1f09fabb49c3968db76e298b1d
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2017-01-14 11:51:00 -0800

    Added where to Guards

--- haskell/guards/Guards.hs
+++ haskell/guards/Guards.hs
@@ -8,5 +8,20 @@ myAbs a                              -- Pattern matched on the (Num a)
       | a < 0     = (-a)             -- The - here is a keyword, not a function
       | otherwise = a
 
+
+-- You can also use where inside of guards
+
+data TicketPrice = Free | Cost Float deriving (Show)
+
+discountCalc :: (Num a, Ord a) => a -> TicketPrice
+discountCalc age
+             | age < 10 = Free
+             | age >= 10 && age < 60 = Cost regular
+             | age >= 60             = Cost senior
+               where regular = 35
+                     senior  = regular - (regular * 0.10)
+
 main = do 
          putStrLn $ show $ myAbs 10
+         putStrLn $ show $ discountCalc 40
+         putStrLn $ show $ discountCalc 80
