7d3bcde29dd14357721fb3353d00687fcfeec1fe
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2017-06-04 07:11:00 -0600

    changed Case to be include all cases

--- haskell/case/Case.hs
+++ haskell/case/Case.hs
@@ -10,12 +10,15 @@ import Util.Assert -- customized library for assertions
 -- compare and contrast with let/Let.hs, and where/Where.hs
 
 myMax :: (Ord a) => [a] -> Maybe a
-myMax xs  = case xs of []  -> Nothing
-                       [x] -> Just x
-                       (x:rest) -> case (myMax $ rest) of
-                            Nothing -> (Just x)
-                            Just a  -> if (a > x) then (Just a)
-                                       else (Just x)
+myMax xs  = case xs of 
+               []  -> Nothing
+               [x] -> Just x
+               (x:rest) ->
+                   case (myMax $ rest) of
+                      Nothing -> Just x
+                      Just a  -> case (a > x) of
+                                   True  -> Just a
+                                   False -> Just x
 
 main = do
          assertThat (myMax [1,5,13,42,5,3,1,-3,100,22,10,-40]) (Just 100)
