3a04153259c714c3ece62aab5b1d3fc4806f852c
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2017-12-18 09:36:48 -0700

    renamed let to lowercase

--- clojure/let/let.clj
+++ clojure/let/let.clj
@@ -1,23 +1,7 @@
 ;Let creates a lexical scope of bindings
-
-(println (let [x 5
-               y 9] (+ x y)))
-
+;
 (assert (= (let [x 5
                  y 9]
-                 (+ x y)) 14))
+                 x + y)) 14)
 
-; x and y cannot be referred to outside of the local scope binding
-;
-(def x 10)
-
-(defn add-x [y] (+ x y))
-
-(println (let [x 6] (add-x 5)))
-
-; see more on dynamic at vars/vars.clj
-(def ^:dynamic x′ 10)
-
-(defn add-x′ [y′] (+ x′ y′))
-
-(println (binding [x′ 6] (add-x′ 5)))
+;x and y cannot be referred to outside of the local scope binding
