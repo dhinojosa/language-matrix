814477915b1c09b8e4fba5e63a6c34b2c762fa85
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2018-02-01 17:15:16 -0700

    added exceptions and README

---
+++ java/exceptions/README.md
@@ -0,0 +1,16 @@
+To run this example:
+
+To compile:
+```
+javac *.java -d target
+```
+
+Run Standard Exceptions Example
+```
+java -cp target com.xyzcorp.StandardExceptions
+```
+
+Run TryWithResources Example
+```
+java -cp target com.xyzcorp.TryWithResources
+```
---
+++ java/exceptions/TryWithResources.java
@@ -0,0 +1,24 @@
+package com.xyzcorp;
+
+import java.io.*;
+import java.net.*;
+
+public class TryWithResources {
+   public static void main(String[] args) {
+      URL url = null;
+      try {
+         url = new URL("https://finance.google.com/finance/historical?output=csv&q=GOOG");
+      } catch (MalformedURLException mue) {
+         mue.printStackTrace();
+      }
+      try ( InputStreamReader reader = new InputStreamReader(url.openStream());
+            BufferedReader bufferedReader = new BufferedReader(reader)) {
+         String line = null;
+         while ((line = bufferedReader.readLine()) != null) {
+            System.out.println(line);
+         }
+      } catch (IOException ioe) {
+         ioe.printStackTrace();
+      }
+   }
+}
