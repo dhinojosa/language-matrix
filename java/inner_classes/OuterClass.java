87a1b6cd949f76eedd42b270fad35dc645fa4fbc
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2019-11-18 13:44:05 -0700

    added inner_classes

---
+++ java/inner_classes/OuterClass.java
@@ -0,0 +1,41 @@
+package com.evolutionnext;
+
+public class OuterClass {
+    private int x = 4;
+    private static int a = 19;
+
+    public class InnerClass {
+        private int y = 2;
+        public int sum() {
+            return x + y;
+        }
+    }
+
+    public static class StaticInnerClass {
+        private int z = 10;
+        public int bar() {
+            return z + 19 + a;
+        }
+    }
+
+    public int foo() {
+        return x + 1;
+    }
+
+    public static int baz() {
+        return a + 10;
+    }
+
+    public int qux(int m) {
+        int g = 10;
+
+        class MyLocalClass {
+            private int art(int n) {
+                return m + n + g;
+            }
+        }
+
+        MyLocalClass myLocalClass = new MyLocalClass();
+        return myLocalClass.art(20);
+    }
+}
---
+++ java/inner_classes/README.md
@@ -0,0 +1,6 @@
+To run this example:
+
+```
+javac BasicThreads.java
+java -ea -cp . BasicThreads
+```
