b50283e7f571c4130d51c76d5ec71a8fa324cdc7
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2020-10-28 13:44:18 -0600

    Moved imports generally fixed NewTypes.hs

--- haskell/newtype/NewTypes.hs
+++ haskell/newtype/NewTypes.hs
@@ -1,12 +1,11 @@
+import Control.Exception.Base (evaluate,assert)
+
 
 -- First we start off with an Algebraic Data Type. 
 -- See record_syntax/RecordSyntax.hs and 
-import Control.Exception.Base (evaluate,assert)
-
 -- algebraic_data_types/AlgebraicDataTypes.hs
-
-
 -- for more information
+
 data Human = Human { firstName :: String, lastName :: String }
 
 -- We can then use a type alias to give it another alias
@@ -16,9 +15,9 @@ type Canadian = Human
 
 -- Here we are applying a Canadian to a function
 -- but this function can also send a Human (see main function)
+
 fullName :: Canadian -> String
 fullName x = (firstName x) ++ " " ++ (lastName x)
-
 
 -- To create a whole new different type on the type system you
 -- can use data as a wrapper with a caveat, 
@@ -44,6 +43,7 @@ usePatternMatchingB (Bulgarian _) = "Got a Bulgarian"
 usePatternMatchingR :: Romanian -> String
 usePatternMatchingR (Romanian _) = "Got a Romanian"
 
+main :: IO ()
 main = do
          putStrLn $ assert ((fullName (Human "Jimmie" "Cleese")) == "Jimmie Cleese") "Assertion 1 passed: ✓"
          putStrLn $ assert ((usePatternMatchingB $ (makeBulgarian "Jim" "Flask")) == "Got a Bulgarian") "Assertion 2 passed: ✓"
