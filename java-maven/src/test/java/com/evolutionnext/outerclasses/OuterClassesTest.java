207017fe2e463dd3370a0983fcf65a188ca5d277
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2020-11-15 12:04:09 -0700

    updated outer, and outerclassestest

--- java-maven/src/test/java/com/evolutionnext/outerclasses/OuterClassesTest.java
+++ java-maven/src/test/java/com/evolutionnext/outerclasses/OuterClassesTest.java
@@ -1,3 +1,28 @@
 package com.evolutionnext.outerclasses;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class OuterClassesTest {
+
+    @Test
+    public void testInnerClasses() {
+        OuterClass outerClass = new OuterClass();
+        OuterClass.InnerClass innerClass = outerClass.new InnerClass();
+        assertEquals(6, innerClass.sum());
+    }
+
+    @Test
+    public void testUsingClassesWithInnerClasses() {
+        OuterClass outerClass = new OuterClass();
+        assertEquals(5, outerClass.foo());
+    }
+
+    @Test
+    public void testUsingClassesWithStaticInnerClasses() {
+        OuterClass.StaticInnerClass staticInnerClass = new OuterClass
+            .StaticInnerClass();
+        assertEquals(48, staticInnerClass.bar());
+    }
 }
