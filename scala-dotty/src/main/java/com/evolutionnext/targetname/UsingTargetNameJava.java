d2b3330f33ee6bd5c1f9a992fe0122205e463e25
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-01-13 12:22:51 -0700

    added target name

---
+++ scala-dotty/src/main/java/com/evolutionnext/targetname/UsingTargetNameJava.java
@@ -0,0 +1,8 @@
+package com.evolutionnext.targetname;
+
+public class UsingTargetNameJava {
+  public static void main(String[] args) {
+    Meters meters = new Meters(30.0);
+    System.out.println(meters.add(new Meters(50)));
+  }
+}
