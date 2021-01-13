fa205fd383d59d8782fcd3c9820622d25afe1890
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-01-13 12:23:15 -0700

    added Match Types

--- scala-dotty/src/main/scala/com/evolutionnext/matchtypes/MatchTypes.scala
+++ scala-dotty/src/main/scala/com/evolutionnext/matchtypes/MatchTypes.scala
@@ -1,14 +1,15 @@
 package com.evolutionnext.matchtypes
 
-object MatchTypes extends App {
-  type Elem[X] = X match {
+object MatchTypes:
+  type Elem[X] = X match
     case String => Char
     case Array[t] => t
     case Iterable[t] => t
-  }
 
-  val m:Elem[String] = 'x'
-  val n:Elem[Array[String]] = "Hello"
-  println(m)
-  println(n.getClass.getSimpleName)
-}
+  @main def testMatchTypes:Unit =
+    val m:Elem[String] = 'x'
+    val n:Elem[Array[String]] = "Hello"
+    println(m)
+    println(n.getClass.getSimpleName)
+
+end MatchTypes
