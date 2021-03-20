f07e91250f1c2411b7918b58ab6b23bdc09e4200
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-03-20 15:40:16 -0600

    added using objects from Scala

---
+++ scala/src/main/java/com.evolutionnext.objects/UsingObjectsFromScala.java
@@ -0,0 +1,7 @@
+package com.evolutionnext.objects;
+
+public class UsingObjectsFromScala {
+    public static void main(String[] args) {
+       System.out.println(MySingleton.add(3, 10));
+    }
+}
