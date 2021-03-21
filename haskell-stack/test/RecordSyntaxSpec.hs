7a879fa4e1388b7a703a6be53d64779267620cb1
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-03-20 19:08:09 -0600

    added all the tests required so far

--- haskell-stack/test/RecordSyntaxSpec.hs
+++ haskell-stack/test/RecordSyntaxSpec.hs
@@ -1,3 +1,5 @@
+module RecordSyntaxSpec (testList) where
+
 -- Using an algebraic type but with record syntax to avoid creating pattern 
 -- matching to get information from the type. In this case various ways to
 -- create an Employee with functions to obtain items.
@@ -8,6 +10,9 @@
 
 import Data.Maybe
 import Control.Exception.Base (assert)
+import Test.HUnit
+import Test.HUnit.Text
+
 
 type FirstName = String
 type LastName = String
@@ -24,29 +29,24 @@ data Employee = Worker {ssn :: SSN, firstName:: FirstName,
                             employees:: [Employee]}
                             deriving (Show)
 
-main = do
-         let johnTheWorkHorse = Worker "333-12-1200" "John"
-                                   Nothing "Smith" ["1234 Amber Lane",
-                                   "St. Louis", "MO"]
+johnTheWorkHorse = 
+  Worker "333-12-1200" "John"
+    Nothing "Smith" ["1234 Amber Lane",
+    "St. Louis", "MO"]
 
-         let carlTheSlouch = Worker{ssn = "001-30-3032",
-                                addresses = ["803 Amber Lane", "Apt B",
-                                            "Little Rock", "AR"],
-                                firstName = "Carl",
-                                middleName = (Just "Landeau"),
-                                lastName = "McInroy"}
+carlTheSlouch = 
+  Worker{ssn = "001-30-3032",
+         addresses = ["803 Amber Lane", "Apt B", "Little Rock", "AR"],
+         firstName = "Carl",
+         middleName = (Just "Landeau"),
+         lastName = "McInroy"}
 
-         let bobTheBoss = Supervisor "124-55-3010" "Bob" Nothing "Ramirez"
-                             ["33 Main St.", "Skokie", "IL"]
-                             [johnTheWorkHorse, carlTheSlouch]
+bobTheBoss = 
+  Supervisor "124-55-3010" "Bob" Nothing "Ramirez"
+    ["33 Main St.", "Skokie", "IL"]
+    [johnTheWorkHorse, carlTheSlouch]
 
-         putStrLn(show(assert(firstName johnTheWorkHorse == "John") "Success"))
-         putStrLn(show(assert(firstName bobTheBoss == "Bob") "Success"))
-         putStrLn(show(assert(firstName carlTheSlouch == "Carl") "Success"))
+testBobIsBob = TestCase (assertEqual "Bob is Bob" "Bob" (firstName bobTheBoss))
+testCarlIsCarl = TestCase (assertEqual "Carl is Carl" "Carl" (firstName carlTheSlouch))
 
-         -- cleaned using function application. See 
-         -- function_application/FunctionApplication.hs for details
-         putStrLn $ show $ assert
-            (firstName johnTheWorkHorse == "John") "Success"
-         putStrLn $ show $ assert (firstName bobTheBoss == "Bob") "Success"
-         putStrLn $ show $ assert (firstName carlTheSlouch == "Carl") "Success"
+testList = TestList [testBobIsBob, testCarlIsCarl]
--- haskell-stack/test/Spec.hs
+++ haskell-stack/test/Spec.hs
@@ -1,7 +1,10 @@
 module Main (main) where
 
 import qualified HigherOrderFunctionsSpec
+import qualified RecordSyntaxSpec
 import Test.HUnit
+import Data.List.Extra
 
 main :: IO Counts
-main = runTestTT $ HigherOrderFunctionsSpec.testList
+main = do runTestTT $ HigherOrderFunctionsSpec.testList
+          runTestTT $ RecordSyntaxSpec.testList
