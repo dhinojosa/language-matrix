3c5f359e5a88dcb84d74d84d68f415d0ddb91a80
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-03-29 11:59:05 -0600

    formatted code with hfmt

--- haskell-stack/test/WhereSpec.hs
+++ haskell-stack/test/WhereSpec.hs
@@ -1,17 +1,15 @@
 module WhereSpec
-  ( testList,
-  )
-where
+  ( testList
+  ) where
 
-import Test.HUnit
+import           Test.HUnit
 
 -- where can be used much like let and with pattern matching
 -- compare and contrast with let/Let.hs
-
 myMax :: (Ord a) => [a] -> Maybe a
 myMax [] = Nothing
 myMax [x] = Just x
-myMax (x : xs)
+myMax (x:xs)
   | item > last = Just item
   | otherwise = Just last
   where
@@ -19,16 +17,32 @@ myMax (x : xs)
     Just last = myMax xs -- Pattern matched here
 
 testMyMax1 :: Test
-testMyMax1 = TestCase (assertEqual "Pattern Matching with Where" (Just 100) (myMax [1, 5, 13, 42, 5, 3, 1, -3, 100, 22, 10, -40]))
+testMyMax1 =
+  TestCase
+    (assertEqual
+       "Pattern Matching with Where"
+       (Just 100)
+       (myMax [1, 5, 13, 42, 5, 3, 1, -3, 100, 22, 10, -40]))
 
 testMyMax2 :: Test
-testMyMax2 = TestCase (assertEqual "Pattern Matching result with empty List" Nothing (myMax ([] :: [Int])))
+testMyMax2 =
+  TestCase
+    (assertEqual
+       "Pattern Matching result with empty List"
+       Nothing
+       (myMax ([] :: [Int])))
 
 testMyMax3 :: Test
-testMyMax3 = TestCase (assertEqual "Pattern Matching result with single element" (Just 1) (myMax [1]))
+testMyMax3 =
+  TestCase
+    (assertEqual
+       "Pattern Matching result with single element"
+       (Just 1)
+       (myMax [1]))
 
 testMyMax4 :: Test
-testMyMax4 = TestCase (assertEqual "Test with 3 elements" (Just 4) (myMax [-3, 4, 1]))
+testMyMax4 =
+  TestCase (assertEqual "Test with 3 elements" (Just 4) (myMax [-3, 4, 1]))
 
 testList :: Test
 testList = TestList [testMyMax1, testMyMax2, testMyMax3, testMyMax4]
