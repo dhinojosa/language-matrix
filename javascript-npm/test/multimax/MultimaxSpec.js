cf057928b0f3ab0ec8d26de812251e35ba59a42d
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-08-19 11:08:29 -0600

    added tests for challenge

--- javascript-npm/test/multimax/MultimaxSpec.js
+++ javascript-npm/test/multimax/MultimaxSpec.js
@@ -15,4 +15,13 @@ describe("Multimax should find the maximum of the array, " +
     it("should return a 10 when the array has two elements, a one and a [10, 5]", () => {
         assert.deepEqual(multimax([1, [10,5]]), 10);
     });
+    it("should return a 10 when the array has three elements, a one, a [10, 5], and a [10, 6]", () => {
+        assert.deepEqual(multimax([1, [10,5], [10, 6]]), 10);
+    });
+    it("should return a 101 when the array has three elements, a one, a [10, 5], and a [10, 101]", () => {
+        assert.deepEqual(multimax([1, [10,5], [10, 101]]), 101);
+    });
+    it("should return a 2001 with this long nested array", () => {
+        assert.deepEqual(multimax([1, [10,5,-1], [10, 101], 2000, [2001, 0]]), 2001);
+    });
 });
