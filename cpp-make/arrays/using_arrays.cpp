6bd09a539a1ed5f491ee4da84882cf1dd34807f7
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-07-03 08:05:00 -0600

    added using_arrays.cpp

--- cpp-make/arrays/using_arrays.cpp
+++ cpp-make/arrays/using_arrays.cpp
@@ -1,3 +1,6 @@
+namespace arrays;
+
+
 #include <iostream>
 using namespace std;
 
@@ -5,15 +8,15 @@ using namespace std;
  * How arrays are created and manipulated in C++
  */
 int main() {
-   int int_array[4] = {1, 2, 3, 4};   //explicit declaration
+    int int_array[4] = {1, 2, 3, 4};   //explicit declaration
 
-   int int_array2[] = {1, 2, 3, 4};   //size of array can be inferred 
-                                      //by declaration 
+    int int_array2[] = {1, 2, 3, 4};   //size of array can be inferred
+    //by declaration
 
-   cout << int_array[2] << "\n";      //access the array
+    cout << int_array[2] << "\n";      //access the array
 
-   int_array[2] = 22;                 //change an element in an array
+    int_array[2] = 22;                 //change an element in an array
 
-   cout << int_array[2] << "\n";      //access the same element of the array
+    cout << int_array[2] << "\n";      //access the same element of the array
 }
 
