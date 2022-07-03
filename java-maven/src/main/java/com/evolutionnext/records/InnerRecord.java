909b2d7ab36659b47d8b4069b7a3b8516bfb30b6
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-07-03 08:03:02 -0600

    added more content on inner record

--- java-maven/src/main/java/com/evolutionnext/records/InnerRecord.java
+++ java-maven/src/main/java/com/evolutionnext/records/InnerRecord.java
@@ -1,3 +1,16 @@
 package com.evolutionnext.records;
+
+import java.util.List;
+
 public class InnerRecord {
+
+    public static void main(String[] args) {
+        record Car(String make, String model, List<String> features){}
+        List<Car> cars = List.of(
+            new Car("Ford", "Focus", List.of()),
+            new Car("Mercedes Benz", "G-Class", List.of("Expensive"))
+        );
+
+        cars.stream().map(c -> c.model).forEach(System.out::println);
+    }
 }
