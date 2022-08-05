9fa68a3202abb843bf26ddb736539761f35b9695
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-08-05 11:22:15 -0600

    added optional braces

--- scala-dotty-sbt-language-matrix/src/main/scala/com/evolutionnext/optionalbraces/OptionalBraces.scala
+++ scala-dotty-sbt-language-matrix/src/main/scala/com/evolutionnext/optionalbraces/OptionalBraces.scala
@@ -1,5 +1,11 @@
-package com.evolutionnext.optionalbraces.OptionalBraces
+package com.evolutionnext.optionalbraces
 
-class OptionalBraces {
+case class Ounces(value: Int):
+    def toMilliliters: Double = value * 29.5735
 
-}
+case class Beverage(name: String, suggestedServing: Ounces, isAlcoholic: Boolean)
+
+@main
+def tryingOutOptionalBraces():Unit =
+  val cocaCola = Beverage("Coca Cola", Ounces(12), false)
+  println(cocaCola)
